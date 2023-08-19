bc58037bcc8be53be6572d4efaf020f356b67fd7
seltzeraddict <kyle.f.tansley@gmail.com>
2023-08-19 19:19:42 -0400

    added opacity filter transition for hamburger

--- Redesign/nav.js
+++ Redesign/nav.js
@@ -1,19 +1,23 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const blogBody = document.querySelector(".blog");
+const hamburgerImg = document.querySelector(".mobile-nav-toggle");
+
 
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible');
 
     if (visibility === "false") {
         primaryNav.setAttribute("data-visible", true);
+        hamburgerImg.style.opacity = .6;
     } else if (visibility === "true") {
         primaryNav.setAttribute("data-visible", false);
+        hamburgerImg.style.opacity = .4;
     }
 
     console.log(visibility);
-
 });
+
 
 blogBody.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible');
