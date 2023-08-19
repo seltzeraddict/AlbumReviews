b85bf39db74365977847e6ab409cda25c6b77a76
seltzeraddict <kyle.f.tansley@gmail.com>
2023-08-19 16:21:33 -0400

    improved JS for menu - click outside to hide

--- Redesign/nav.js
+++ Redesign/nav.js
@@ -1,5 +1,6 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const blogBody = document.querySelector(".blog");
 
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible');
@@ -13,3 +14,14 @@ navToggle.addEventListener('click', () => {
     console.log(visibility);
 
 });
+
+blogBody.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === "false") {
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+    }
+
+    console.log(visibility);
+})
